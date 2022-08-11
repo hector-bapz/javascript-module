e9677f58f5bedce38e750028b6302e56ddfaf32b
Hector <Hector.bprz@gmail.com>
2022-08-10 21:49:52 -0500

    fix format

--- sesion1/estructurasDeControl.js
+++ sesion1/estructurasDeControl.js
@@ -1,3 +1,5 @@
+//CLASE
+
 let numero1 = 2
 let numero2 = 2
 let resultado = true
@@ -6,8 +8,8 @@ resultado = numero1>numero2;
 
 if(numero1==numero2){
     console.log("ninguno de los 2 son mayores");
-}else if(resultado)
+} else if(resultado){
     console.log(numero1, " es mayor que ", numero2);
-else{
+} else{
     console.log(numero2, " es mayor que ", numero1);
 }
