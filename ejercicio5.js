8f1f047348f353ead808e8edf8b5b5a4e4f18968
Hector <Hector.bprz@gmail.com>
2022-08-15 21:08:45 -0500

    fix exercise 5

--- ejercicio5.js
+++ ejercicio5.js
@@ -1,14 +1,21 @@
 let array = [10, 2, 2, 10, 2, 2, 5, 10];
-let value = 2;
+let value = true;
 
 function countingArray(array, value) {
     let count=0;
+
+    if(typeof value !== "number") {
+     console.log("porfavor escriba un numero");
+     return;
+    }
+
     for (i=0; i <= array.length-1; i++) {
-        if(value==array[i]){
+        if(value===array[i]) {
             count++;
         }
     }
     return count;
+    
 }
 
 let count= countingArray(array, value);
