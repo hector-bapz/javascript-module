cc85378ff7d6ee916bc92eb715c69ba82761afd2
Hector <Hector.bprz@gmail.com>
2022-09-12 00:17:08 -0500

    opimizing code pt1

--- tictactoe/js/app.js
+++ tictactoe/js/app.js
@@ -1,27 +1,22 @@
-const topLeft = document.querySelector("#topLeft");
-const topCenter = document.querySelector("#topCenter");
-const topRight = document.querySelector("#topRight");
-const midLeft = document.querySelector("#midLeft");
-const midCenter = document.querySelector("#midCenter");
-const midRight = document.querySelector("#midRight");
-const bottomLeft = document.querySelector("#bottomLeft");
-const bottomCenter = document.querySelector("#bottomCenter");
-const bottomRight = document.querySelector("#bottomRight");
-const reset = document.querySelector("#reset");
-const select = document.querySelector("#selection");
-const instruction = document.querySelector("#instruction");
+// import * as variables from "./js";
 const myModal = document.querySelector("#winnerModal");
 const modalBody = document.querySelector("#modalBody");
 const modalTitle = document.querySelector("#modalTitle");
 const closeModalX = document.querySelector("#closeModalX");
 const closeModalBtn = document.querySelector("#closeModalBtn");
-let gameStarted = 0;
+const reset = document.querySelector("#reset");
+const select = document.querySelector("#selection");
+const instruction = document.querySelector("#instruction");
 let currentPiece = "X";
+let gameStarted = 0;
 let xClass = "";
 let oClass = "";
 
-function selectValidate(id) {
-  if (id == "btn") {
+const btns = document.querySelectorAll(".btn-ttt");
+
+function selectValidate(element, id) {
+  // console.log(element);
+  if (id === "btn") {
     if (select.hidden === false) {
       select.hidden = true;
       instruction.innerText = "Let's Game!";
@@ -30,44 +25,19 @@ function selectValidate(id) {
     if (select.hidden === true) {
       select.hidden = false;
       instruction.innerText = "Select your piece!";
-      topLeft.innerText = "";
-      topCenter.innerText = "";
-      topRight.innerText = "";
-      midLeft.innerText = "";
-      midCenter.innerText = "";
-      midRight.innerText = "";
-      bottomLeft.innerText = "";
-      bottomCenter.innerText = "";
-      bottomRight.innerText = "";
-      resetClass(topLeft, "reset");
-      resetClass(topCenter, "reset");
-      resetClass(topRight, "reset");
-      resetClass(midLeft, "reset");
-      resetClass(midCenter, "reset");
-      resetClass(midRight, "reset");
-      resetClass(bottomLeft, "reset");
-      resetClass(bottomCenter, "reset");
-      resetClass(bottomRight, "reset");
-      topLeft.setAttribute("clicked", "false");
-      topCenter.setAttribute("clicked", "false");
-      topRight.setAttribute("clicked", "false");
-      midLeft.setAttribute("clicked", "false");
-      midCenter.setAttribute("clicked", "false");
-      midRight.setAttribute("clicked", "false");
-      bottomLeft.setAttribute("clicked", "false");
-      bottomCenter.setAttribute("clicked", "false");
-      bottomRight.setAttribute("clicked", "false");
+      // resetClass(element, "reset");
+      element.setAttribute("data-clicked", "false");
       currentPiece = select.value;
     }
   }
 }
 
 function setValue(id, whoIsCalling) {
-  if (id.getAttribute("clicked") === "false") {
+  if (id.getAttribute("data-clicked") === "false") {
     if (currentPiece === "O") {
       id.innerText = currentPiece;
       if (whoIsCalling === "btn") {
-        id.setAttribute("clicked", "true");
+        id.setAttribute("data-clicked", "true");
         currentPiece = "X";
         id.classList.add("oValue");
       } else {
@@ -76,7 +46,7 @@ function setValue(id, whoIsCalling) {
     } else {
       id.innerText = currentPiece;
       if (whoIsCalling === "btn") {
-        id.setAttribute("clicked", "true");
+        id.setAttribute("data-clicked", "true");
         currentPiece = "O";
         id.classList.add("xValue");
       } else {
@@ -84,24 +54,25 @@ function setValue(id, whoIsCalling) {
       }
     }
   }
+  console.log(game);
 }
 
-function resetClass(id, whoIsCalling) {
-  if (whoIsCalling === "reset") {
-    xClass = "xValue";
-    oClass = "oValue";
-  } else {
-    xClass = "ghost--xValue";
-    oClass = "ghost--oValue";
-  }
-  for (item in id.classList) {
-    if (id.classList[item] == xClass) {
-      id.classList.remove(xClass);
-    } else if (id.classList[item] == oClass) {
-      id.classList.remove(oClass);
-    }
-  }
-}
+// function resetClass(id, whoIsCalling) {
+//   if (whoIsCalling === "reset") {
+//     xClass = "xValue";
+//     oClass = "oValue";
+//   } else {
+//     xClass = "ghost--xValue";
+//     oClass = "ghost--oValue";
+//   }
+//   for (let item in id.classList) {
+//     if (id.classList[item] == xClass) {
+//       id.classList.remove(xClass);
+//     } else if (id.classList[item] == oClass) {
+//       id.classList.remove(oClass);
+//     }
+//   }
+// }
 
 function setModal(winner) {
   myModal.classList.add("show");
@@ -117,129 +88,138 @@ function setModal(winner) {
   }
 }
 
-function isThereAWinner() {
-  if (
-    topLeft.innerText === "X" &&
-    topCenter.innerText === "X" &&
-    topRight.innerText === "X"
+let game = {
+  topLeft: "",
+  topCenter: "",
+  topRight: "",
+  midLeft: "",
+  midCenter: "",
+  midRight: "",
+  bottomLeft: "",
+  bottomCenter: "",
+  bottomRight: "",
+};
+
+function isThereAWinner(element) {
+  if (game.topLeft === "X" && game.topCenter === "X" && game.topRight === "X") {
+    setModal("X");
+    console.log(element);
+    selectValidate(element, "reset");
+  } else if (
+    game.midLeft === "X" &&
+    game.midCenter === "X" &&
+    game.midRight === "X"
   ) {
     setModal("X");
     selectValidate("reset");
   } else if (
-    midLeft.innerText === "X" &&
-    midCenter.innerText === "X" &&
-    midRight.innerText === "X"
+    game.bottomLeft === "X" &&
+    game.bottomCenter === "X" &&
+    game.bottomRight === "X"
   ) {
     setModal("X");
     selectValidate("reset");
   } else if (
-    bottomLeft.innerText === "X" &&
-    bottomCenter.innerText === "X" &&
-    bottomRight.innerText === "X"
+    game.topLeft === "X" &&
+    game.midCenter === "X" &&
+    game.bottomRight === "X"
   ) {
     setModal("X");
     selectValidate("reset");
   } else if (
-    topLeft.innerText === "X" &&
-    midCenter.innerText === "X" &&
-    bottomRight.innerText === "X"
+    game.topLeft === "X" &&
+    game.midLeft === "X" &&
+    game.bottomLeft === "X"
   ) {
     setModal("X");
     selectValidate("reset");
   } else if (
-    topLeft.innerText === "X" &&
-    midLeft.innerText === "X" &&
-    bottomLeft.innerText === "X"
+    game.topCenter === "X" &&
+    game.midCenter === "X" &&
+    game.bottomCenter === "X"
   ) {
     setModal("X");
     selectValidate("reset");
   } else if (
-    topCenter.innerText === "X" &&
-    midCenter.innerText === "X" &&
-    bottomCenter.innerText === "X"
+    game.topRight === "X" &&
+    game.midRight === "X" &&
+    game.bottomRight === "X"
   ) {
     setModal("X");
     selectValidate("reset");
   } else if (
-    topRight.innerText === "X" &&
-    midRight.innerText === "X" &&
-    bottomRight.innerText === "X"
+    game.topRight === "X" &&
+    game.midCenter === "X" &&
+    game.bottomLeft === "X"
   ) {
     setModal("X");
     selectValidate("reset");
   } else if (
-    topRight.innerText === "X" &&
-    midCenter.innerText === "X" &&
-    bottomLeft.innerText === "X"
-  ) {
-    setModal("X");
-    selectValidate("reset");
-  } else if (
-    topLeft.innerText === "O" &&
-    topCenter.innerText === "O" &&
-    topRight.innerText === "O"
+    game.topLeft === "O" &&
+    game.topCenter === "O" &&
+    game.topRight === "O"
   ) {
     setModal("O");
     selectValidate("reset");
   } else if (
-    midLeft.innerText === "O" &&
-    midCenter.innerText === "O" &&
-    midRight.innerText === "O"
+    game.midLeft === "O" &&
+    game.midCenter === "O" &&
+    game.midRight === "O"
   ) {
     setModal("O");
     selectValidate("reset");
   } else if (
-    bottomLeft.innerText === "O" &&
-    bottomCenter.innerText === "O" &&
-    bottomRight.innerText === "O"
+    game.bottomLeft === "O" &&
+    game.bottomCenter === "O" &&
+    game.bottomRight === "O"
   ) {
     setModal("O");
     selectValidate("reset");
   } else if (
-    topLeft.innerText === "O" &&
-    midCenter.innerText === "O" &&
-    bottomRight.innerText === "O"
+    game.topLeft === "O" &&
+    game.midCenter === "O" &&
+    game.bottomRight === "O"
   ) {
     setModal("O");
     selectValidate("reset");
   } else if (
-    topLeft.innerText === "O" &&
-    midLeft.innerText === "O" &&
-    bottomLeft.innerText === "O"
+    game.topLeft === "O" &&
+    game.midLeft === "O" &&
+    game.bottomLeft === "O"
   ) {
     setModal("O");
     selectValidate("reset");
   } else if (
-    topCenter.innerText === "O" &&
-    midCenter.innerText === "O" &&
-    bottomCenter.innerText === "O"
+    game.topCenter === "O" &&
+    game.midCenter === "O" &&
+    game.bottomCenter === "O"
   ) {
     setModal("O");
     selectValidate("reset");
   } else if (
-    topRight.innerText === "O" &&
-    midRight.innerText === "O" &&
-    bottomRight.innerText === "O"
+    game.topRight === "O" &&
+    game.midRight === "O" &&
+    game.bottomRight === "O"
   ) {
     setModal("O");
     selectValidate("reset");
   } else if (
-    topRight.innerText === "O" &&
-    midCenter.innerText === "O" &&
-    bottomLeft.innerText === "O"
+    game.topRight === "O" &&
+    game.midCenter === "O" &&
+    game.bottomLeft === "O"
   ) {
     setModal("O");
     selectValidate("reset");
   } else if (
-    topLeft.innerText !== "" &&
-    topCenter.innerText !== "" &&
-    topRight.innerText !== "" &&
-    midLeft.innerText !== "" &&
-    midCenter.innerText !== "" &&
-    midRight.innerText !== "" &&
-    bottomLeft.innerText !== "" &&
-    bottomCenter.innerText !== "" &&
-    bottomRight.innerText !== ""
+    game.topLeft !== "" &&
+    game.topCenter !== "" &&
+    game.topRight !== "" &&
+    game.midLeft !== "" &&
+    game.midCenter !== "" &&
+    game.midRight !== "" &&
+    game.bottomLeft !== "" &&
+    game.bottomCenter !== "" &&
+    game.bottomRight !== ""
   ) {
     setModal("");
     selectValidate("reset");
@@ -247,7 +227,7 @@ function isThereAWinner() {
 }
 
 function clickHappened(id) {
-  if (id.getAttribute("clicked") === "false") {
+  if (id.getAttribute("data-clicked") === "false") {
     id.innerHTML = "";
   }
 }
@@ -256,132 +236,144 @@ select.addEventListener("change", () => {
   currentPiece = select.value;
 });
 
-topLeft.addEventListener("click", (event) => {
-  setValue(event.target, "btn");
-  selectValidate("btn");
-  isThereAWinner();
+btns.forEach((button) => {
+  button.addEventListener("click", clickbtn);
+  // button.addEventListener('mouseover', );
 });
 
-topLeft.addEventListener("mouseover", (event) => {
-  setValue(event.target, "mouseover");
-});
+function clickbtn() {
+  game[this.id] = currentPiece;
+  setValue(this, "btn");
+  selectValidate(this, "btn");
+  isThereAWinner(this);
+}
 
-topLeft.addEventListener("mouseout", (event) => {
-  resetClass(event.target, "mouseout");
-  clickHappened(event.target);
-});
+// topLeft.addEventListener("click", (event) => {
+//   setValue(event.target, "btn");
+//   selectValidate("btn");
+//   isThereAWinner();
+// });
 
-topCenter.addEventListener("click", (event) => {
-  setValue(event.target, "btn");
-  selectValidate("btn");
-  isThereAWinner();
-});
-topCenter.addEventListener("mouseover", (event) => {
-  setValue(event.target, "mouseover");
-});
+// topLeft.addEventListener("mouseover", (event) => {
+//   setValue(event.target, "mouseover");
+// });
 
-topCenter.addEventListener("mouseout", (event) => {
-  resetClass(event.target, "mouseout");
-  clickHappened(event.target);
-});
+// topLeft.addEventListener("mouseout", (event) => {
+//   resetClass(event.target, "mouseout");
+//   clickHappened(event.target);
+// });
 
-topRight.addEventListener("click", (event) => {
-  setValue(event.target, "btn");
-  selectValidate("btn");
-  isThereAWinner();
-});
-topRight.addEventListener("mouseover", (event) => {
-  setValue(event.target, "mouseover");
-});
+// topCenter.addEventListener("click", (event) => {
+//   setValue(event.target, "btn");
+//   selectValidate("btn");
+//   isThereAWinner();
+// });
+// topCenter.addEventListener("mouseover", (event) => {
+//   setValue(event.target, "mouseover");
+// });
 
-topRight.addEventListener("mouseout", (event) => {
-  resetClass(event.target, "mouseout");
-  clickHappened(event.target);
-});
+// topCenter.addEventListener("mouseout", (event) => {
+//   resetClass(event.target, "mouseout");
+//   clickHappened(event.target);
+// });
 
-midLeft.addEventListener("click", (event) => {
-  setValue(event.target, "btn");
-  selectValidate("btn");
-  isThereAWinner();
-});
-midLeft.addEventListener("mouseover", (event) => {
-  setValue(event.target, "mouseover");
-});
+// topRight.addEventListener("click", (event) => {
+//   setValue(event.target, "btn");
+//   selectValidate("btn");
+//   isThereAWinner();
+// });
+// topRight.addEventListener("mouseover", (event) => {
+//   setValue(event.target, "mouseover");
+// });
 
-midLeft.addEventListener("mouseout", (event) => {
-  resetClass(event.target, "mouseout");
-  clickHappened(event.target);
-});
+// topRight.addEventListener("mouseout", (event) => {
+//   resetClass(event.target, "mouseout");
+//   clickHappened(event.target);
+// });
 
-midCenter.addEventListener("click", (event) => {
-  setValue(event.target, "btn");
-  selectValidate("btn");
-  isThereAWinner();
-});
-midCenter.addEventListener("mouseover", (event) => {
-  setValue(event.target, "mouseover");
-});
+// midLeft.addEventListener("click", (event) => {
+//   setValue(event.target, "btn");
+//   selectValidate("btn");
+//   isThereAWinner();
+// });
+// midLeft.addEventListener("mouseover", (event) => {
+//   setValue(event.target, "mouseover");
+// });
 
-midCenter.addEventListener("mouseout", (event) => {
-  resetClass(event.target, "mouseout");
-  clickHappened(event.target);
-});
+// midLeft.addEventListener("mouseout", (event) => {
+//   resetClass(event.target, "mouseout");
+//   clickHappened(event.target);
+// });
 
-midRight.addEventListener("click", (event) => {
-  setValue(event.target, "btn");
-  selectValidate("btn");
-  isThereAWinner();
-});
-midRight.addEventListener("mouseover", (event) => {
-  setValue(event.target, "mouseover");
-});
+// midCenter.addEventListener("click", (event) => {
+//   setValue(event.target, "btn");
+//   selectValidate("btn");
+//   isThereAWinner();
+// });
+// midCenter.addEventListener("mouseover", (event) => {
+//   setValue(event.target, "mouseover");
+// });
 
-midRight.addEventListener("mouseout", (event) => {
-  resetClass(event.target, "mouseout");
-  clickHappened(event.target);
-});
+// midCenter.addEventListener("mouseout", (event) => {
+//   resetClass(event.target, "mouseout");
+//   clickHappened(event.target);
+// });
 
-bottomLeft.addEventListener("click", (event) => {
-  setValue(event.target, "btn");
-  selectValidate("btn");
-  isThereAWinner();
-});
-bottomLeft.addEventListener("mouseover", (event) => {
-  setValue(event.target, "mouseover");
-});
+// midRight.addEventListener("click", (event) => {
+//   setValue(event.target, "btn");
+//   selectValidate("btn");
+//   isThereAWinner();
+// });
+// midRight.addEventListener("mouseover", (event) => {
+//   setValue(event.target, "mouseover");
+// });
 
-bottomLeft.addEventListener("mouseout", (event) => {
-  resetClass(event.target, "mouseout");
-  clickHappened(event.target);
-});
+// midRight.addEventListener("mouseout", (event) => {
+//   resetClass(event.target, "mouseout");
+//   clickHappened(event.target);
+// });
 
-bottomCenter.addEventListener("click", (event) => {
-  setValue(event.target, "btn");
-  selectValidate("btn");
-  isThereAWinner();
-});
-bottomCenter.addEventListener("mouseover", (event) => {
-  setValue(event.target, "mouseover");
-});
+// bottomLeft.addEventListener("click", (event) => {
+//   setValue(event.target, "btn");
+//   selectValidate("btn");
+//   isThereAWinner();
+// });
+// bottomLeft.addEventListener("mouseover", (event) => {
+//   setValue(event.target, "mouseover");
+// });
 
-bottomCenter.addEventListener("mouseout", (event) => {
-  resetClass(event.target, "mouseout");
-  clickHappened(event.target);
-});
+// bottomLeft.addEventListener("mouseout", (event) => {
+//   resetClass(event.target, "mouseout");
+//   clickHappened(event.target);
+// });
 
-bottomRight.addEventListener("click", (event) => {
-  setValue(event.target, "btn");
-  selectValidate("btn");
-  isThereAWinner();
-});
-bottomRight.addEventListener("mouseover", (event) => {
-  setValue(event.target, "mouseover");
-});
+// bottomCenter.addEventListener("click", (event) => {
+//   setValue(event.target, "btn");
+//   selectValidate("btn");
+//   isThereAWinner();
+// });
+// bottomCenter.addEventListener("mouseover", (event) => {
+//   setValue(event.target, "mouseover");
+// });
 
-bottomRight.addEventListener("mouseout", (event) => {
-  resetClass(event.target, "mouseout");
-  clickHappened(event.target);
-});
+// bottomCenter.addEventListener("mouseout", (event) => {
+//   resetClass(event.target, "mouseout");
+//   clickHappened(event.target);
+// });
+
+// bottomRight.addEventListener("click", (event) => {
+//   setValue(event.target, "btn");
+//   selectValidate("btn");
+//   isThereAWinner();
+// });
+// bottomRight.addEventListener("mouseover", (event) => {
+//   setValue(event.target, "mouseover");
+// });
+
+// bottomRight.addEventListener("mouseout", (event) => {
+//   resetClass(event.target, "mouseout");
+//   clickHappened(event.target);
+// });
 
 reset.addEventListener("click", (event) => {
   selectValidate("reset");
